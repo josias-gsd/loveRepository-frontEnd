f0998e4a573a202db522b2f19f2edd5da983894c
Josias Gentil <josiasgentil7@gmail.com>
2025-11-05 00:13:49 +0000

    actualizado love-6

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,7 @@
 import axios from "axios";
-const api = axios.create({ baseURL: "https://josiasaby.netlify.app/" });
+const api = axios.create({
+  baseURL:
+    "https://love-repository-git-master-josias-projects-aff3abc7.vercel.app/",
+});
 
 export default api;
