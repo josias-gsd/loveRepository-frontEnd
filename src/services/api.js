9bcfb9f10903e2c4a42b9fb730a605695846ea62
Josias Gentil <josiasgentil7@gmail.com>
2025-11-05 00:19:48 +0000

    actualizado love-8

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "https://love-repository.vercel.app/",
+  baseURL:
+    "https://love-repository-kbj3g0n2z-josias-projects-aff3abc7.vercel.app/",
 });
 
 export default api;
